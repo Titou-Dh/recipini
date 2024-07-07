8b1225e148da9b7fb7c447cb0ee97017ee464dab
Titou-Dh <titoudh1@gmail.com>
2024-07-08 00:40:53 +0100

    fixed client id issue with google provider

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -28,7 +28,7 @@ const handler =  NextAuth({
         //     }
         // }),
         GoogleProvider ({
-            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
