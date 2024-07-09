9d1499cdda00f1041f485bbac643c63106cda0b2
Titou-Dh <titoudh1@gmail.com>
2024-07-09 06:10:04 +0100

    update ProfileCard.jsx

--- components/community/ProfileCard.jsx
+++ components/community/ProfileCard.jsx
@@ -9,21 +9,24 @@ import { Button } from '../ui/button';
 
 function ProfileCard() {
     const { data: session } = useSession()
+    const name = session?.user.username
+    const email = session?.user.email
+    const image = session?.user.image
 
     return (
         <div className="bg-[#ffffff5e] rounded-lg border dark:border-backgroundL   shadow-lg p-4 z-30  lg:fixed h-min">
             <div className="flex justify-between items-center gap-4">
                 <div className="flex items-center">
-                    <Image src={session?.user.image} alt="profile" width={50} height={50} className="w-12 h-12 rounded-full" />
+                    <Image src={image} alt="profile" width={50} height={50} className="w-12 h-12 rounded-full" />
                     <div className="ml-2">
-                        <h2 className="text-lg font-semibold">{session?.user.name}</h2>
-                        <p className="text-sm text-gray-600">{session?.user.email}</p>
+                        <h2 className="text-lg font-semibold">{name}</h2>
+                        <p className="text-sm text-gray-600">{email}</p>
                     </div>
                 </div>
                 <Button className="bg-primaryL dark:hover:text-gray-900  text-white px-4 py-2 rounded-lg">Edit Profile</Button>
             </div>
             <div className="mt-4">
-                <p className="text-sm text-gray-900">Welcome back {session?.user.name} 👋</p>
+                <p className="text-sm text-gray-900">Welcome back {name} 👋</p>
             </div>
         </div>
     )
