c556a0665e490ce7c3118f79077a62c72b3ab770
Titou-Dh <titoudh1@gmail.com>
2024-07-11 23:45:14 +0100

    update route.js

---
+++ app/api/post/create/route.js
@@ -0,0 +1,45 @@
+
+import multer from 'multer'; // For image upload handling
+import connectDB from '@/utils/connectDB';
+import Recipe from '@/models/Recipe';
+
+
+
+// Multer configuration
+const upload = multer({dist: 'public/images'});
+
+// POST /api/post/create
+
+const handler = async () => {
+    try {
+        // Connect to database
+        await connectDB();
+
+        const uploadImage = util.promisify(upload.single('image'));
+        await uploadImage(req, res);
+
+        console.log(req.body);
+        console.log(req.file);
+        
+
+        // Get user from database
+        const newRecipe = new Recipe({
+            title: req.body.title,
+            description: req.body.description,
+            ingredients: req.body.ingredients,
+            instructions: req.body.instructions,
+            image: req.file ? `/images/${req.file.filename}` : '',
+            author: req.body.author,
+        });
+
+        // Save recipe to database
+        await newRecipe.save();
+
+        return new Response(200).json({ message: 'Recipe created successfully' });
+    }
+    catch (error) {
+        return new Response(500).json({ message: 'Error creating recipe' });
+    }
+};
+
+export {handler as POST}
