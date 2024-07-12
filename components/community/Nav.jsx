d684be5983a3d1902f8e889e3fed7afe78f17e2c
Titou-Dh <titoudh1@gmail.com>
2024-07-12 03:03:50 +0100

    added logout to the nav

--- components/community/Nav.jsx
+++ components/community/Nav.jsx
@@ -8,14 +8,7 @@ import { signOut } from "next-auth/react";
 
 function Nav() {
 
-    const handleLogout = () => {
-        async () => {
-            await signOut({
-                redirect: false,
-                callbackUrl: '/'
-            });
-        }
-    };
+
     return (
         <div className='md:w-max z-50 w-[90%] fixed bottom-5 mx-auto left-1/2 transform -translate-x-1/2 navbar-blur border px-4 py-2 rounded-xl dark:bg-[#ffffff0b]'>
             <div className="flex items-center justify-around md:justify-between gap-4">
@@ -39,8 +32,8 @@ function Nav() {
                     <FontAwesomeIcon icon={faUser} className="h-6 w-6" />
                     <span className="hidden md:flex">Profile</span>
                 </Link>
-                <Link href="#" onClick={handleLogout}  className="text-lg hover:scale-[1.1] transition-all font-medium hover:underline underline-offset-4 flex gap-2 dark:text-white" prefetch={false}>
-                    <FontAwesomeIcon icon={faSignOut} className="h-6 w-6"  />
+                <Link href="#" onClick={() => signOut({callbackUrl:"/login"})} className="text-lg hover:scale-[1.1] transition-all font-medium hover:underline underline-offset-4 flex gap-2 dark:text-white" prefetch={false}>
+                    <FontAwesomeIcon icon={faSignOut} className="h-6 w-6" />
                     <span className="hidden md:flex">Logout</span>
                 </Link>
                 <DarkModeToggle />
